a8de934368fa2896d1a937c5929dadfce006cd4c
A1ex <blinov.alex.s@gmail.com>
2019-09-13 00:29:32 +0700

    Add project files.

---
+++ TopShelfTest/GeneratedProxy/IService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopShelfTest
+{
+
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    [System.ServiceModel.ServiceContractAttribute(ConfigurationName = "IService")]
+    public interface IService
+    {
+
+        [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IService/PutReceipt", ReplyAction = "http://tempuri.org/IService/PutReceiptResponse")]
+        void PutReceipt(Receipt receipt);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IService/PutReceipt", ReplyAction = "http://tempuri.org/IService/PutReceiptResponse")]
+        System.Threading.Tasks.Task PutReceiptAsync(Receipt receipt);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IService/GetReceipts", ReplyAction = "http://tempuri.org/IService/GetReceiptsResponse")]
+        void GetReceipts(int number);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IService/GetReceipts", ReplyAction = "http://tempuri.org/IService/GetReceiptsResponse")]
+        System.Threading.Tasks.Task GetReceiptsAsync(int number);
+    }
+}
---
+++ TopShelfTest/GeneratedProxy/ServiceClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopShelfTest
+{
+
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    public class ServiceClient : System.ServiceModel.ClientBase<IService>, IService
+    {
+
+        public ServiceClient()
+        {
+        }
+
+        public ServiceClient(string endpointConfigurationName) :
+                base(endpointConfigurationName)
+        {
+        }
+
+        public ServiceClient(string endpointConfigurationName, string remoteAddress) :
+                base(endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public ServiceClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) :
+                base(endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public ServiceClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) :
+                base(binding, remoteAddress)
+        {
+        }
+
+        public void PutReceipt(Receipt receipt)
+        {
+            base.Channel.PutReceipt(receipt);
+        }
+
+        public System.Threading.Tasks.Task PutReceiptAsync(Receipt receipt)
+        {
+            return base.Channel.PutReceiptAsync(receipt);
+        }
+
+        public void GetReceipts(int number)
+        {
+            base.Channel.GetReceipts(number);
+        }
+
+        public System.Threading.Tasks.Task GetReceiptsAsync(int number)
+        {
+            return base.Channel.GetReceiptsAsync(number);
+        }
+    }
+}
